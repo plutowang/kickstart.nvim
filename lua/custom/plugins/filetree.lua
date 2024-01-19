b8b9eff34773a541437c5e0d60f44de612c0b480
Bochao Wang <bwang@eckler.ca>
2024-01-19 10:58:03 -0500

    add toggleterm

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -7,7 +7,36 @@ return {
     "MunifTanjim/nui.nvim",
   },
   keys = {
-    { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" }
+    {
+      "<leader>re",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = "/" })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (cwd)",      remap = true },
+    {
+      "<leader>ge",
+      function()
+        require("neo-tree.command").execute({ source = "git_status", toggle = true })
+      end,
+      desc = "Git explorer",
+    },
+    {
+      "<leader>be",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", toggle = true })
+      end,
+      desc = "Buffer explorer",
+    },
   },
   config = function()
     require('neo-tree').setup({
@@ -29,7 +58,7 @@ return {
       window = {
         position = "left",
         width = "15%",
-        auto_expand_width = true,
+        auto_expand_width = false,
       },
     })
   end,
---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,59 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+        {
+          "<leader>t",
+          function()
+            require("toggleterm").toggle(1, 0, vim.loop.cwd(), "horizontal")
+          end,
+          desc = "Horizontal Terminal",
+        },
+        {
+            "<leader>t2",
+            function()
+              require("toggleterm").toggle(2, 0, vim.loop.cwd(), "horizontal")
+            end,
+            desc = "Horizontal Terminal",
+          },
+        {
+            "<leader>tf",
+            function()
+              require("toggleterm").toggle(1, 0, vim.loop.cwd(), "float")
+            end,
+            desc = "Horizontal Terminal",
+        },
+        {
+            "<leader>tv",
+            function()
+              require("toggleterm").toggle(1, 0, vim.loop.cwd(), "vertical")
+            end,
+            desc = "Vertical Terminal",
+        },
+        {
+          "<leader>Tn",
+          "<cmd>ToggleTermSetName<cr>",
+          desc = "Set Terminal Name",
+        },
+        {
+          "<leader>Ts",
+          "<cmd>TermSelect<cr>",
+          desc = "Select Terminal",
+        },
+    },
+    config = function()
+        require('toggleterm').setup({
+            shading_factor = 0.3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+            size = 10,
+            open_mapping = [[<F7>]],
+            direction = "float",
+            float_opts = {
+                border = "curved",
+                highlights = {
+                    border = "Normal",
+                    background = "Normal",
+                },
+            },
+        })
+    end,
+}

23dc798035e44c655abd85da0eab016aadbb2712
Bochao Wang <bwang@eckler.ca>
2024-01-24 21:13:29 -0500

    config colorscheme

--- init.lua
+++ init.lua
@@ -194,6 +194,20 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup {
+        style = 'light',
+        transparent = false,
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+
+         -- toggle theme style ---
+        toggle_style_key = "<leader>ss", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+      }
       vim.cmd.colorscheme 'onedark'
     end,
   },

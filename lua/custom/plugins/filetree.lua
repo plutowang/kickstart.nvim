427ef1859e7d8dbd5958f5f4467972e2c31cb424
Bochao Wang <bwang@eckler.ca>
2024-01-27 04:36:39 -0500

    adjust config

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -46,8 +46,25 @@ return {
           enabled = true,
         },
         filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            "node_modules"
+          },
+          hide_by_pattern = { -- uses glob style patterns
+            "*/src/*/tsconfig.json",
+          },
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".gitignored",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            "thumbs.db"
+          },
+          never_show_by_pattern = { -- uses glob style patterns
+            ".null-ls_*",
+          },
         },
       },
       close_if_last_window = false,

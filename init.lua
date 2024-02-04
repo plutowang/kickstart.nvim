f33b5cad2dc1deef2f73950d65af3715ab1d669b
plutowang <pluto.wbc@gmail.com>
2024-02-04 18:02:35 -0500

    config zls

--- init.lua
+++ init.lua
@@ -462,7 +462,7 @@ vim.defer_fn(function()
     },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
+    auto_install = true,
 
     highlight = { enable = true },
     indent = { enable = true },
@@ -661,8 +661,19 @@ local servers = {
   },
   tailwindcss = {},
   tsserver = {},
+  zls = {
+    enable_build_on_save = true,
+    enable_autofix = true,
+    enable_argument_placeholders = true,
 
-
+    enable_inlay_hints = true,
+    inlay_hints_show_variable_type_hints = true,
+    inlay_hints_show_parameter_name = true,
+    inlay_hints_show_builtin = true,
+    inlay_hints_exclude_single_argument = false,
+    inlay_hints_hide_redundant_param_names = false,
+    inlay_hints_hide_redundant_param_names_last_token = false,
+  },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },

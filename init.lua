e9f28ebaf016abda4953e9163b611b0ee6990457
plutowang <pluto.wbc@gmail.com>
2024-05-07 00:20:44 -0400

    upate config also support windows

--- init.lua
+++ init.lua
@@ -318,12 +318,17 @@ require('lazy').setup({
 
         -- `build` is used to run some command when the plugin is installed/updated.
         -- This is only run then, not every time Neovim starts up.
-        build = 'make',
+        build = (function()
+          if (vim.fn.has 'win32' == 1 and vim.fn.executable 'cmake' == 1) then
+            return 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+          end
+          return 'make'
+        end)(),
 
         -- `cond` is a condition used to determine whether this plugin should be
         -- installed and loaded.
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable 'make' == 1 or (vim.fn.has 'win32' == 1 and vim.fn.executable 'cmake' == 1)
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },

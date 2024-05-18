a2e8e950406a25c5ed4e32d876cc8f4f7c20fc14
Bochao Wang <bwang@eckler.ca>
2024-05-18 14:19:53 -0400

    autosave when lost focus

--- init.lua
+++ init.lua
@@ -1154,3 +1154,11 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'CursorHoldI', 'FocusGai
   command = "if mode() != 'c' | checktime | endif",
   pattern = { '*' },
 })
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost' }, {
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand '%' ~= '' and vim.bo.buftype == '' then
+      vim.api.nvim_command 'silent update'
+    end
+  end,
+})

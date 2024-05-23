f77e3ec85cd6683f5c60edbb6dbfe89a4ad6731b
Bochao Wang <bwang@eckler.ca>
2024-05-23 10:40:51 -0400

    add graphql and nvim-treesitter-textobjects

--- init.lua
+++ init.lua
@@ -677,6 +677,7 @@ require('lazy').setup({
             },
           },
         },
+        graphql = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -902,48 +903,48 @@ require('lazy').setup({
   --  end,
   --},
 
-   {
-     "catppuccin/nvim",
-     name = "catppuccin",
-     priority = 1000,
-     config = function()
-       vim.opt.background = 'light'
-       require("catppuccin").setup({
-         flavour = "auto", -- latte, frappe, macchiato, mocha
-         background = { -- :h background
-             light = "latte",
-             dark = "frappe",
-         },
-         transparent_background = true, -- disables setting the background color.
-         show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-         dim_inactive = {
-             enabled = false, -- dims the background color of inactive window
-             shade = "dark",
-             percentage = 0.15, -- percentage of the shade to apply to the inactive window
-         },
-         no_italic = false, -- Force no italic
-         no_bold = false, -- Force no bold
-         no_underline = false, -- Force no underline
-         default_integrations = true,
-         integrations = {
-             cmp = true,
-             gitsigns = true,
-             nvimtree = true,
-             treesitter = true,
-             notify = false,
-             mini = {
-                 enabled = true,
-                 indentscope_color = "",
-             },
-             barbar = true,
-             mason = true,
-             neotree = true,
-         },
-       })
-       vim.cmd.colorscheme "catppuccin"
-     end
-   },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.opt.background = 'light'
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'frappe',
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          barbar = true,
+          mason = true,
+          neotree = true,
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
 
   {
     -- Set lualine as statusline
@@ -1151,7 +1152,6 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'CursorHoldI', 'FocusGai
   command = "if mode() != 'c' | checktime | endif",
   pattern = { '*' },
 })
-
 
 -- Auto save
 vim.opt.autoread = true

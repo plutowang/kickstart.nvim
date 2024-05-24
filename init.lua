cbb62889434d95a12b19df0e7c8a36f948474ec8
Bochao Wang <bwang@eckler.ca>
2024-05-24 11:24:26 -0400

    update lualine.nvim repo

--- init.lua
+++ init.lua
@@ -947,7 +947,7 @@ require('lazy').setup({
 
   {
     -- Set lualine as statusline
-    'nvim-klualine/lualine.nvim',
+    'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     opts = {
       options = {
@@ -995,17 +995,17 @@ require('lazy').setup({
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
+      -- local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      --statusline.section_location = function()
+      --  return '%2l:%-2v'
+      --end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim

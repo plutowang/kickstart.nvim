e6a43745d88d9d6446b34cb05354151e66fcddff
plutowang <pluto.wbc@gmail.com>
2024-05-24 01:38:01 -0400

    update hardtime config

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -2,6 +2,31 @@ return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
-        disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" }
+        disabled_filetypes = {
+            "NvimTree",
+            "TelescopePrompt",
+            "aerial",
+            "alpha",
+            "checkhealth",
+            "dapui*",
+            "Diffview*",
+            "Dressing*",
+            "help",
+            "httpResult",
+            "lazy",
+            "Neogit*",
+            "mason",
+            "neotest-summary",
+            "minifiles",
+            "neo-tree",
+            "netrw",
+            "noice",
+            "notify",
+            "prompt",
+            "qf",
+            "oil",
+            "undotree",
+            "Trouble",
+        }
     }
 }

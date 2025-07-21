7605c0790507cbf642ba1734bb0a09b3e0972f11
Bochao Wang <bwang@eckler.ca>
2025-07-21 11:08:09 -0400

    update the model

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -1,57 +1,57 @@
 return {
-  "yetone/avante.nvim",
+  'yetone/avante.nvim',
   -- If there is issues, try:
-    -- 1. Delete ~/.local/share/nvim/lazy/avante.nvim
-    -- 2. Start nvim, :Lazy + sync
-    -- 3. Build manually:
-    --    - cd ~/.local/share/nvim/lazy/avante.nvim
-    --    - make
+  -- 1. Delete ~/.local/share/nvim/lazy/avante.nvim
+  -- 2. Start nvim, :Lazy + sync
+  -- 3. Build manually:
+  --    - cd ~/.local/share/nvim/lazy/avante.nvim
+  --    - make
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   -- ⚠️ must add this setting! ! !
   build = function()
     -- conditionally use the correct build system for the current OS
-    if vim.fn.has("win32") == 1 then
-      return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    if vim.fn.has 'win32' == 1 then
+      return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
     else
-      return "make"
+      return 'make'
     end
   end,
-  event = "VeryLazy",
+  event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
   ---@module 'avante'
   ---@type avante.Config
   opts = {
     -- add any opts here
     -- for example
-    provider = "ollama",
+    provider = 'ollama',
     providers = {
       ollama = {
-        endpoint = "http://localhost:11434",
-        model = "codestral:latest",
+        endpoint = 'http://localhost:11434',
+        model = 'starcoder2:instruct',
         timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
       },
     },
   },
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "stevearc/dressing.nvim", -- for input provider dressing
-    "folke/snacks.nvim", -- for input provider snacks
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua', -- for file_selector provider fzf
+    'stevearc/dressing.nvim', -- for input provider dressing
+    'folke/snacks.nvim', -- for input provider snacks
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
       -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
       opts = {
         -- recommended settings
         default = {
@@ -69,9 +69,10 @@ return {
       -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
       opts = {
-        file_types = { "markdown", "Avante" },
+        file_types = { 'markdown', 'Avante' },
       },
-      ft = { "markdown", "Avante" },
+      ft = { 'markdown', 'Avante' },
     },
   },
 }
+

06a51e6554b5824be4b740d96aa8f34030f03a97
plutowang <wbc.pluto@gmail.com>
2025-12-08 01:40:23 -0500

    feat: enhance gitsigns configuration with additional keymaps and event triggers

--- init.lua
+++ init.lua
@@ -278,18 +278,19 @@ require('lazy').setup({
   -- options to `gitsigns.nvim`.
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- Already defined in lua/kickstart/plugins/gitsigns.lua
+  -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   'lewis6991/gitsigns.nvim',
+  --   opts = {
+  --     signs = {
+  --       add = { text = '+' },
+  --       change = { text = '~' },
+  --       delete = { text = '_' },
+  --       topdelete = { text = '‾' },
+  --       changedelete = { text = '~' },
+  --     },
+  --   },
+  -- },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -983,6 +984,7 @@ require('lazy').setup({
         'L3MON4D3/LuaSnip',
         version = '2.*',
         lazy = true,
+        event = { 'InsertEnter' },
         build = (function()
           -- Build Step is needed for regex support in snippets.
           -- This step is not supported in many windows environments.

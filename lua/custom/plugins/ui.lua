7abba1376aeffee0194ad92578ddee7ee5e1a949
plutowang <wbc.pluto@gmail.com>
2025-12-08 22:48:53 -0500

    feat: move catppuccin theme and lualine configuration in custom/plugins/ui.lua

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.opt.background = 'light'
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'frappe',
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          barbar = true,
+          mason = true,
+          neotree = true,
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'catppuccin',
+        component_separators = '|',
+        section_separators = '',
+        extensions = { 'lazy', 'mason', 'neo-tree', 'nvim-dap-ui', 'toggleterm' },
+      },
+      -- sections = {
+      --  lualine_x = {
+      --    "rest"
+      --  }
+      -- }
+    },
+  },
+}
